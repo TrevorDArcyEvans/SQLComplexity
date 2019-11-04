1639a868c274e5bac89c9ee934b84e6e0490d298
Trevor D'Arcy-Evans <trevor.darcy-evans@easysoft.com>
2019-11-04 16:44:01 +0000

    - capture top level events

--- SQLComplexity/TSqlParserListener.cs
+++ SQLComplexity/TSqlParserListener.cs
@@ -5,5 +5,14 @@ namespace SQLComplexity
   public sealed class TSqlParserListener : TSqlParserBaseListener
   {
     // TODO  listen to events --> complexity
+    public override void EnterTsql_file(TSqlParser.Tsql_fileContext context)
+    {
+      
+    }
+
+    public override void ExitTsql_file(TSqlParser.Tsql_fileContext context)
+    {
+      
+    }
   }
 }

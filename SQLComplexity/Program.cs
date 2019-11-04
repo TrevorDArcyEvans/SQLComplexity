b09de55cce4127d936c7412fcf772f0fe2346cae
Trevor D'Arcy-Evans <trevor.darcy-evans@easysoft.com>
2019-11-04 14:48:30 +0000

    - added command line project

---
+++ SQLComplexity/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using SQLParser;
+using SQLParser.Enums;
+
+namespace SQLComplexity
+{
+  public static class Program
+  {
+    public static void Main(string[] args)
+    {
+      if (args.Length != 1)
+      {
+        Usage();
+        return;
+      }
+
+      var sql = File.ReadAllText(args[0]);
+      var listener = new TSqlParserListener();
+      Parser.Parse(sql, listener, SQLType.TSql);
+
+      // TODO    output complexity
+    }
+
+    private static void Usage()
+    {
+      Console.WriteLine("Usage:");
+      Console.WriteLine("  SQLComplexity.exe [path_to_sql_file]");
+      Console.WriteLine();
+      Console.WriteLine("Example:");
+      Console.WriteLine("  SQLComplexity.exe CreateDatabase.sql");
+    }
+  }
+}
---
+++ SQLComplexity/TSqlParserListener.cs
@@ -0,0 +1,9 @@
+using SQLParser.Parsers.TSql;
+
+namespace SQLComplexity
+{
+  public sealed class TSqlParserListener : TSqlParserBaseListener
+  {
+    // TODO  listen to events --> complexity
+  }
+}

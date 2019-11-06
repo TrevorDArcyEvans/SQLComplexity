08feb6c4aacc38d5c09c4632b6492461e8b1cb22
Trevor D'Arcy-Evans <trevor.darcy-evans@easysoft.com>
2019-11-06 09:58:01 +0000

    - output node info in mainline

--- SQLComplexity/Program.cs
+++ SQLComplexity/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
+using Gma.DataStructures;
 using SQLParser.Enums;
 using Parser = SQLParser.Parser;
 
@@ -32,13 +33,20 @@ namespace SQLComplexity
       //    https://en.wikipedia.org/wiki/Dense_graph
       //    https://www.quora.com/How-do-you-compute-the-density-of-a-weighted-graph
       var startNode = listener.EnterContext;
-      var allNodes = new HashSet<IParseTree>();
+      var allNodes = new OrderedSet<IParseTree>();
       Dump(startNode.Depth(), allNodes, startNode);
       Console.WriteLine();
 
       var leafNodes = allNodes.Where(x => x.ChildCount == 0);
       var allDepths = leafNodes.Select(x => x.GetDepth());
       var length = allDepths.Sum();
+
+      foreach (var node in allNodes)
+      {
+        var newDepth = (node as RuleContext)?.Depth() ?? node.GetDepth() + 1;
+        var padding = new string(' ', newDepth);
+        Console.WriteLine($"{padding} [{newDepth}@{node.GetType().Name}] {node}");
+      }
 
       Console.WriteLine($"Analysed [{args[0]}] in {sw.ElapsedMilliseconds} ms");
       Console.WriteLine($"  MaxDepth   = {allDepths.Max()}");
@@ -49,22 +57,18 @@ namespace SQLComplexity
 
     // RuleContext : IParseTree, ISyntaxTree, ITree
     // TerminalNodeImpl : IParseTree, ISyntaxTree, ITree
-    private static void Dump(int depth,
-      ISet<IParseTree> allNodes,
+    private static void Dump(
+      int depth,
+      ICollection<IParseTree> allNodes,
       IParseTree node)
     {
       var newDepth = (node as RuleContext)?.Depth() ?? depth + 1;
-      var padding = new string(' ', newDepth);
-//      if (node is TerminalNodeImpl)
-      {
-        Console.WriteLine($"{padding} [{newDepth}@{node.GetType().Name}] {node}");
-      }
 
       if (!allNodes.Contains(node))
       {
         allNodes.Add(node);
       }
-      
+
       for (var i = 0; i < node.ChildCount; i++)
       {
         var child = node.GetChild(i);

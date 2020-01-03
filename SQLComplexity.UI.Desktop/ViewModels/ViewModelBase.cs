da6ba2271cb989ebf982b39e09c38c120b50814a
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-01-03 21:41:25 +0000

    - added UI

---
+++ SQLComplexity.UI.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace SQLComplexity.UI.Desktop.ViewModels
+{
+  public class ViewModelBase : ReactiveObject
+  {
+  }
+}
